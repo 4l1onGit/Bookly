8f3d28d3d154be9d8e78c58d4ba6ec3ff720dbfa
Hassan <hali@hasalidev.online>
2026-01-23 09:22:51 +0000

    Add NelmioApiDocBundle for API documentation

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
